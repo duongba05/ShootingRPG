519e355ab51ea2c71ca0be666d01e4dda4b4b7ad
pesduo <pesduo05@gmail.com>
2025-04-08 16:13:35 +0700

    testGame

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button PlayBtn;
+    public Button OptionBtn;
+    public Button ExitBtn;
+    void Start()
+    {
+        PlayBtn.onClick.AddListener(PlayGame);
+        ExitBtn.onClick.AddListener(ExitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    void OptionsGame()
+    {
+        Debug.Log("oPTIONS");
+    }
+    void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
